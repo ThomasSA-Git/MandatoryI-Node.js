eb768c08a0c6cc19788a75b66d982df653cbbdb6
ThomasSA-Git <thom45w9@stud.kea.dk>
2023-09-21 12:26:18 +0200

    sidebar, notes and stylesheet added

--- app.js
+++ app.js
@@ -2,27 +2,34 @@
 import express from "express";
 
 // Instantiate express
-const app = express();
+const app = express();  
+
+// Imports the 'path' module which provides utilities for working with file and directory paths.
+import path from "path";
+
+// Enables JSON parsing for incoming requests,
+// allowing the application to handle JSON data in the request body.
+app.use(express.json());
+
+// This line of code serves static files from the "public" directory, making them accessible to clients.
+// Important as security measure. This serves static files securely.
+// Only gives access to intented files and prevents unauthorized access to static files in public folder.
+app.use(express.static("public"));
 
 // Set port number
 const PORT = 8080;
 
 // Set app to listen to port number
-app.listen(PORT, () => {
-    console.log("Server is running on port:", PORT);
+app.listen(PORT, () => console.log("Server is running on port:", PORT));
+
+
+// CRUD
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve("public/frontpage.html"));
 });
 
-// Enables JSON parsing for incoming requests,
-// allowing the application to handle JSON data in the request body.
-app.use(express.json)
-
-// This line of code serves static files from the "public" directory, making them accessible to clients.
-// Important as security measure. This serves static files securely.
-// Only gives access to intented files and prevents unauthorized access to static files.
-app.use(express.static("public"));
-
 app.get("/", (req, res) => {
-
+    
 });
 
 app.get("/", (req, res) => {
@@ -40,3 +47,39 @@ app.patch("/", (req, res) => {
 app.delete("/", (req, res) => {
     
 })
+
+
+/* fetch explained 
+function getPokemon() {
+  // Step 1: Initiating the Fetch as a get request
+  fetch("https://pokeapi.co/api/v2/")
+
+    // Step 2: Fetching Data as a Byte Stream
+    .then((response) => {
+      // Step 3: Parsing the Byte Stream as JSON
+      return response.json();
+    })
+
+    // Step 4: Accessing the JSON Data
+    .then((data) => {
+      if (data.error) {
+        // Step 5a: Handling Errors in the JSON Data
+        console.log(data.error);
+      } else {
+        // Step 5b: Returning the Data (if no error)
+        return data;
+      }
+    })
+
+    // Step 6: Handling Errors in the Fetch and Parsing Process
+    .catch((error) => {
+      console.log("Error occurred: ", error);
+    });
+
+  // Step 7: Returning a Promise (either with data or undefined)
+  // Note: This function doesn't return the data directly; it returns a Promise.
+  // The data will be available through the resolved Promise in the calling code.
+}
+
+
+ */
---
+++ public/sidebar.js
@@ -0,0 +1,30 @@
+const sideBar = document.getElementById("sidebar");
+
+sideBar.innerHTML = `
+<ul>
+<li><a href="#">1. Home</a></li>
+<li><a href="#">2. Node.js and express</a></li>
+<li><a href="#">3. REST API</a></li>
+<li><a href="#">4. Data types</a></li>
+<li><a href="#">5. Functions</a></li>
+<li><a href="#">6. Loops</a></li>
+<li><a href="#">7. Fetch</a></li>
+<li><a href="#">8. Git</a></li>
+<li><a href="#">9. XSS</a></li>
+<li><a href="#">10. CSS and HTML</a></li>
+<li><a href="#">11. Admin login</a></li>
+<li><a href="#">12. About me</a></li>
+</ul>`;
+
+
+// 1 Short description
+// 2 Short description of what both is, how to install (commands) and how they complement eachother.
+// 3 Show an example of a REST API, explain the conventions (2 of them) when designing an full CRUD API.
+// 4 Show and explain datatypes with examples.
+// 5 Show and explain functions shown in class with examples.
+// 6 map, find, filter, findByIndex?, sort, reduce, foreach and so on.
+// 7 Show an example of fetch and explain step by step what happens.
+// 8 Show the basic git commands we've been using.
+// 9 Explain XSS and show an example of what to do and what not to do. Explain the concept.
+// 10 Inline style, topfile style or seperate stylesheet. Show examples and explain what is best and why. Also explain circular dependency (problems with linking to several scripts in the html page)
+// 
